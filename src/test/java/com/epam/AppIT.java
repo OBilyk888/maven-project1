b309d5057076d66b9d7c94fcfcd7a200bddd559d
Olha Bilyk <Olha_Bilyk@epam.com>
2020-02-07 14:29:22 +0200

    Initial commit

---
+++ src/test/java/com/epam/AppIT.java
@@ -0,0 +1,20 @@
+package com.epam;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppIT
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
